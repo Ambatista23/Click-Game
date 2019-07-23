17ce463371aece8638f9e67225c4ce42c40868f8
Andre Batista <andrebatista@Andres-MacBook-Air.local>
2019-07-22 21:15:17 -0400

    Updates

---
+++ precache-manifest.3f3d9865a48f0760dc0ab858d1063ef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f543a84fb199c65b157b340d28b79da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "156d1ab41a67ef190941",
+    "url": "/static/css/main.28819c70.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "156d1ab41a67ef190941",
+    "url": "/static/js/main.c7618716.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
